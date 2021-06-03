322045eee87821bdabbaf94f874b8703643cc762
Aleksa <milenkovicaleksa94@yahoo.com>
2021-06-03 03:39:38 +0200

    Implementation of Event emitter for communication between components

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -6,44 +6,100 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import CategoryPage from '../CategoryPage/CategoryPage';
 import ContactPage from '../ContactPage/ContactPage';
+import EventRegister from '../../api/EventRegister';
+import api from '../../api/api';
 
-function Application() {
-  return (
-    <BrowserRouter>
-      <Container className="Application">
-        <div className="Application-header">
-          Front-end aplikacije
-        </div>
-        <TopMenu />
-        <div className="Application-body">
-          <Switch>
-            <Route exact path="/" component={ HomePage } />
-
-            <Route path="/category/:cid?"
-              render={
-                (props: any) => {
-                  return (
-                    <CategoryPage { ...props } />
-                  )
-                }
-              }
-            />
-
-            <Route path="/contact">
-              <ContactPage title="Our location in Belgrade" phone="+381 11 462 44 75" address="Sarajevska 14" />
-            </Route>
-
-            <Route path="/profile">
-              My profile
-            </Route>
-          </Switch>
-        </div>
-        <div>
-          &copy; 2021...
-        </div>
-      </Container>
-    </BrowserRouter>
-  );
+class ApplicationState {
+  authorizedRole: "user" | "administrator" | "visitor" = "visitor";
 }
 
-export default Application;
+export default class Application extends React.Component {
+  state: ApplicationState;
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      authorizedRole: 'visitor'
+    }
+  }
+
+  componentDidMount() {
+    EventRegister.on("AUTH_EVENT", this.authEventHandler.bind(this));
+
+    this.checkRole("user");
+    this.checkRole("administrator");
+  }
+
+  componentWillUnmount() {
+    EventRegister.off("AUTH_EVENT", this.authEventHandler.bind(this));
+  }
+
+  private authEventHandler(message: string) {
+    console.log('Application: authEventHandler: ', message);
+
+    if (message === "force_login" || message === "user_logout" || message === "administrator_logout") {
+      return this.setState({ authorizedRole: "visitor" });
+    }
+
+    if (message === "user_login") {
+      return this.setState({ authorizedRole: "user" });
+    }
+
+    if (message === "administrator_login") {
+      return this.setState({ authorizedRole: "administrator" });
+    }
+  }
+
+  private checkRole(role: "user" | "administrator") {
+    api("GET", "/auth/" + role + "/ok", role)
+      .then(res => {
+        if (res?.data === "OK") {
+          this.setState({
+            authorizedRole: role,
+          });
+          EventRegister.emit("AUTH_EVENT", role + "_login");
+        }
+      })
+      .catch(() => {});
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Container className="Application">
+          <div className="Application-header">
+            Front-end aplikacije
+          </div>
+          <TopMenu currentMenuType={ this.state.authorizedRole } />
+          <div className="Application-body">
+            <Switch>
+              <Route exact path="/" component={ HomePage } />
+
+              <Route path="/category/:cid?"
+                     render={
+                       (props: any) => {
+                         return (
+                           <CategoryPage { ...props } />
+                         )
+                       }
+                     }
+              />
+
+              <Route path="/contact">
+                <ContactPage title="Our location in Belgrade" phone="+381 11 462 44 75" address="Sarajevska 14" />
+              </Route>
+
+              <Route path="/profile">
+                My profile
+              </Route>
+            </Switch>
+          </div>
+          <div>
+            &copy; 2021...
+          </div>
+        </Container>
+      </BrowserRouter>
+    );
+  }
+}
